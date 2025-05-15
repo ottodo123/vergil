75107d0cc941228d00723387bfb265bb67d3998d
Risekk37 <ktkim8461@gmail.com>
2025-05-15 15:46:03 -0400

    ds

---
+++ config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAmZrFMnXgBipBNgNFCMOASxfNmOY1VWJw",
+    authDomain: "vergil-4e5ca.firebaseapp.com",
+    projectId: "vergil-4e5ca",
+    storageBucket: "vergil-4e5ca.appspot.com",
+    messagingSenderId: "135292455436",
+    appId: "1:135292455436:web:1d28a0c00fa6f88c173c29",
+    measurementId: "G-63XNPHWS3E"
+};
